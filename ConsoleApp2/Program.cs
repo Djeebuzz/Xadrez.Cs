a57c874971a6cf02a0b2ba613b8837e783dbe28b
djeebuzz <eugenio.silva9010@gmail.com>
2026-03-02 16:06:54 +0000

    iniciando pelo tabuleiro

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Tabuleiro;
+
+namespace Xadrez {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Posicao P;
+            P = new Posicao(3,4);
+
+
+            Console.WriteLine( "Posicao: " + P);
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
