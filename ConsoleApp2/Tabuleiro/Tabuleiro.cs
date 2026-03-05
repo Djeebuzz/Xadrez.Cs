af779217a9ce6f55b7754675926ccd945fd77407
--global <eugeniomsilva90@gmail.com>
2026-03-05 20:45:20 +0000

    just to be safe

--- ConsoleApp2/Tabuleiro/Tabuleiro.cs
+++ ConsoleApp2/Tabuleiro/Tabuleiro.cs
@@ -19,6 +19,16 @@ namespace tabuleiro
         {
             return pecas[linhas, colunas];
         }
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha,pos.coluna];
+        }
+        public bool existePeca (Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null; 
+        }
+
 
 
         public void ColocarPeca(Peca p, Posicao pos)
@@ -26,5 +36,24 @@ namespace tabuleiro
             pecas[pos.linha,pos.coluna] = p;
             p.posicao = pos;
         }
+
+        public bool posicaoValida (Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna < colunas)
+            {
+                return false;
+            }
+            return true;
+        }  
+        public void validarPosicao (Posicao pos)
+        {
+            if(!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posicao Invalida");
+            }
+        }
+
+
+
     }
 }
--- ConsoleApp2/obj/Debug/net8.0/Xadrez-Console.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net8.0/Xadrez-Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Xadrez-Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48114a45dfaa02a582a8442ded520e7e0059951e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d14c7cf1312715b0f9e6743c186da2d006110e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Xadrez-Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Xadrez-Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
